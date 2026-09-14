cc61fe0895c57d3ed6b268a032ffd949b7a51d18
Jomari Macaraig <jomari@auxahelth.com>
2026-09-15 01:47:52 +0800

    Create content app and update settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -42,7 +42,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -50,6 +50,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+THIRD_PARTY_APPS = []
+
+LOCAL_APPS = ["apps.content"]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
